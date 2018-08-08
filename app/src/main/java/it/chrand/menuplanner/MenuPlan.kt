644bb263158189a1ef21feefd5102ef8469d8690
itchrand <ac_is_nowhere@yahoo.com>
2018-08-08 10:11:07 +0200

    Initial commit

---
+++ app/src/main/java/it/chrand/menuplanner/MenuPlan.kt
@@ -0,0 +1,12 @@
+package it.chrand.menuplanner
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuPlan : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_plan)
+    }
+}
